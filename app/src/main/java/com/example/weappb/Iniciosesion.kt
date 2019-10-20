2b11c0d6b9beb4b98224de7c6f4c03a4e951ea16
KTDRAL <angel.catedral.1410@gmail.com>
2019-10-20 16:34:12 -0500

    PutoElQueLoLea

--- app/src/main/java/com/example/weappb/Iniciosesion.kt
+++ app/src/main/java/com/example/weappb/Iniciosesion.kt
@@ -11,6 +11,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
+//xd
+
 class Iniciosesion : AppCompatActivity(){
 
     private lateinit var txtUser:EditText
